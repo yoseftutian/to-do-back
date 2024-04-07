d649ceb2f18bb69a06732703baf7ee25850ed775
yosef-tutian <yoseftutian@gmail.com>
2024-04-07 12:56:32 +0300

    conect to mongo

---
+++ database.mjs
@@ -0,0 +1,32 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri =
+  "mongodb+srv://yoseftutian:05484gps@shklimcluster.w6r058d.mongodb.net/?retryWrites=true&w=majority&appName=ShklimCluster";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+const db = cluster.db("totahim");
+export const todosCollection = db.collection("todos");
+export default db;

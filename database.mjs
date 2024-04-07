5843c6dddec771682d8fefac8d890bddca390783
yosef-tutian <yoseftutian@gmail.com>
2024-04-07 14:14:12 +0300

    cors jwt

--- database.mjs
+++ database.mjs
@@ -11,21 +11,16 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+let cluster;
+
+try {
+  // Connect the client to the server	(optional starting in v4.7)
+  cluster = await client.connect();
+  // Send a ping to confirm a successful connection
+} catch (error) {
+  console.error(error);
+  // Ensures that the client will close when you finish/error
 }
-run().catch(console.dir);
 
 const db = cluster.db("totahim");
 export const todosCollection = db.collection("todos");
--- index.mjs
+++ index.mjs
@@ -1,9 +1,23 @@
 import express from "express";
+import Todos from "./todos.mjs";
+import { expressjwt as jwt } from "express-jwt";
+
 const app = express();
 const port = 3005;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(json());
+app.use(cors());
+app.use(
+  jwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"],
+  }).unless({ path: ["/users/login", "/users/register"] })
+);
+app.use("/todos", Todos);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(err.message);
 });
 
 app.listen(port, () => {
